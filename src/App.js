615e825a842ac97e6a306ddbf469d1bdd9f5d0b5
Micheala-H <mholthus17@gmail.com>
2023-06-08 23:39:24 -0500

    added more to index, app.js and some homepage js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import homepage from './pages/homepage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if ( currentPage === 'homepage') {
+      return <homepage />;
+    }
+  }
+  function App() {
+    return (
+      <div>
+        <homepage />
+      </div>
+
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/homepage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function homepage() {
+    return(
+        <div className=""> 
+            <h1>Micheala Holthus</h1>
+            <p>I am a new full stack developer</p>
+        </div>
+    );
+}
